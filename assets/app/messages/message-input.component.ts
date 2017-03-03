9dc562dd6367ca181a7293690a0adf962498eb35
Henry Au <henryau01@gmail.com>
2017-03-03 02:21:05 -0800

    add messages with a service

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,13 +1,20 @@
+import { Message } from './message.model';
+import { MessageService } from './message.service';
 import { Component } from '@angular/core';
 
 
 @Component({
     selector: 'app-message-input',
-    templateUrl: './message-input.component.html'
+    templateUrl: './message-input.component.html',
+    providers: [MessageService]
 })
 
 export class MessageInputComponent {
+    constructor(private messageService: MessageService) {}
+
     onSave(value: string) {
-        console.log(value);
+        const message = new Message(value, 'Henry');
+        this.messageService.addMessage(message);
+        
     }
 }
