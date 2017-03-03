fc5c7c5d4043b7ad661dc6656c16cb53e6594bd4
Henry Au <henryau01@gmail.com>
2017-03-02 21:45:25 -0800

    fetch data from db

--- routes/app.js
+++ routes/app.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('node');
+    // {} just finds first
+    User.findOne({}, function(err, doc) {
+        if(err) {
+            return res.send('Error!');
+        }
+        // just fetch email
+        res.render('node', {email: doc.email});
+    });
 });
 
 router.post('/message', function (req, res, next) {
-    var message = req.body.message;
-    res.redirect('/message/' + message);
+    var email = req.body.email;
+    var user = new User({
+        firstName: 'Henry',
+        lastName: 'Au',
+        password: 'mapassword', // encrypt later
+        email: email
+    });
+    user.save();
+    res.redirect('/');
 });
 
 module.exports = router;
