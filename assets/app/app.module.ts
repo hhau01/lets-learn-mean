392b55fed0a1fce451b27c87d1b7af3d665d4b24
Henry Au <henryau01@gmail.com>
2017-03-02 23:04:32 -0800

    multiple components

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,12 +1,15 @@
+import { AppComponent } from './app.component';
+import { MessageComponent } from './messages/message.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
+
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        MessageComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
