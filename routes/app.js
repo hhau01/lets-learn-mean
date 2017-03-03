1feb1bd0ff64d4bb0cf6001dbf06fbd7369d1b68
Henry Au <henryau01@gmail.com>
2017-03-02 22:02:14 -0800

    start angular2

--- routes/app.js
+++ routes/app.js
@@ -1,28 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    // {} just finds first
-    User.findOne({}, function(err, doc) {
-        if(err) {
-            return res.send('Error!');
-        }
-        // just fetch email
-        res.render('node', {email: doc.email});
-    });
-});
-
-router.post('/message', function (req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Henry',
-        lastName: 'Au',
-        password: 'mapassword', // encrypt later
-        email: email
-    });
-    user.save();
-    res.redirect('/');
+    res.render('index');
 });
 
 module.exports = router;
