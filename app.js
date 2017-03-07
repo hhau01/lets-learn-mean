95a4eed483090d5fc6d8f1027d05f65ade5ec82c
Henry Au <henryau01@gmail.com>
2017-03-06 23:04:25 -0800

    set up backend for signup

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 
 var appRoutes = require('./routes/app');
 var messageRoutes = require('./routes/messages');
-
+var userRoutes = require('./routes/user');
 
 var app = express();
 mongoose.connect('localhost:27017/node-angular');
@@ -34,6 +34,7 @@ app.use(function (req, res, next) {
 
 // arrange by specificity
 app.use('/message', messageRoutes);
+app.use('/user', userRoutes);
 app.use('/', appRoutes);
 
 // catch 404 and forward to error handler
